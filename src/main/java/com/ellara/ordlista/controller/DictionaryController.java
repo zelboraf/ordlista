4ab45b26d8e784bac87e2182e2c374df48cedea2
zelboraf <uminski.krzysztof@gmail.com>
2023-11-17 07:21:09 +0100

    searchstring to lowercase version 0.0.9

--- src/main/java/com/ellara/ordlista/controller/DictionaryController.java
+++ src/main/java/com/ellara/ordlista/controller/DictionaryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpSession;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Controller
@@ -70,6 +71,7 @@ public class DictionaryController {
             model.addAttribute(dictionary);
             return "edit";
         }
+        searchString = searchString.toLowerCase(Locale.ROOT);
         setHomeView(model, searchString, dictionaryLang);
         session.setAttribute("searchString", searchString);
         return "/home";
